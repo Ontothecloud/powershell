26fb85cdc0d2edf36e304d84a6aaa30fd86651cd
OntoTheCloud <96190623+Ontothecloud@users.noreply.github.com>
2023-04-27 07:47:30 -0400

    Add files via upload

---
+++ Cleanup_Disable_Account.ps1
@@ -0,0 +1,2 @@
+﻿$When = ((Get-Date).AddDays(-90)).Date
+Get-ADUser -Filter {LastLogonDate -lt $When} -Properties * | select-object samaccountname,givenname,surname,LastLogonDate | Disable-ADAccount
